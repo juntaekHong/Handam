457b534e71d46297718103482f5ac455ce71456f
juntae shin <wnsxo0816@gmail.com>
2019-07-08 01:01:14 +0900

    회원가입3 작업중
    선택 뷰 작업중

--- src/containers/sign/SignUp3.js
+++ src/containers/sign/SignUp3.js
@@ -1,14 +1,27 @@
 import React from "react";
 import { connect } from "react-redux";
-import { BaseView, Title } from "../../components/common/View";
-import { Step } from "../../components/signup/View";
+import { BaseView, Title, HCenterView } from "../../components/common/View";
+import { Step, SelectView } from "../../components/signup/View";
+import { SignUpNext } from "../../components/signup/Button";
+import { NBGBText } from "../../components/common/Text";
+import colors from "../../configs/colors";
 
 const SignUp3 = props => {
   return (
-    <BaseView>
+    <HCenterView>
       <Title title="회원가입" />
       <Step number={3} />
-    </BaseView>
+      <HCenterView>
+        <SelectView subject={"전공(1트랙)-필수"} value={"선택하기"} />
+        <SelectView subject={"복수전공(2트랙)"} value={"선택하기"} />
+        <SelectView subject={"부전공"} value={"선택하기"} />
+        <SelectView subject={"연계전공"} value={"선택하기"} />
+        <SelectView subject={"입학년도-필수"} value={"선택하기"} />
+      </HCenterView>
+      <SignUpNext>
+        <NBGBText color={colors.white}>시작하기</NBGBText>
+      </SignUpNext>
+    </HCenterView>
   );
 };
 
