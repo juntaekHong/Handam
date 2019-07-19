c651f6a955f59ac4a51f4420c60e0be86b9e78c3
juntae shin <wnsxo0816@gmail.com>
2019-07-19 21:25:44 +0900

    알람 리스트 화면 완성(게시물 네비게이션 빼고)

--- src/containers/alarm/Alarm.js
+++ src/containers/alarm/Alarm.js
@@ -1,15 +1,26 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import { connect } from "react-redux";
 import { BaseView, Title } from "../../components/common/View";
-import { AlarmTab } from "../../components/alarm/AlarmTab";
+import { AlarmTab } from "../../components/alarm/view/AlarmTab";
+import { AlarmList } from "../../components/alarm/view/AlarmList";
+import { AlarmActions, CommonActions } from "../../store/actionCreator";
 
 const Alarm = props => {
   const [index, setIndex] = useState(0);
   const handleUnread = useCallback(() => {
-    setIndex(0);
+    listCall(0);
   }, []);
   const handleRead = useCallback(() => {
-    setIndex(1);
+    listCall(1);
+  }, []);
+  const listCall = useCallback(async value => {
+    await CommonActions.handleLoading(true);
+    setIndex(value);
+    await AlarmActions.getAlarmList(value === 0 ? false : true, 0);
+    await CommonActions.handleLoading(false);
+  });
+  useEffect(() => {
+    listCall(0);
   }, []);
   return (
     <BaseView>
@@ -18,9 +29,19 @@ const Alarm = props => {
         index={index}
         handleUnread={handleUnread}
         handleRead={handleRead}
+        count={props.count}
       />
+      {index == 0 ? (
+        <AlarmList list={props.unreadList} index={index} />
+      ) : (
+        <AlarmList list={props.readList} index={index} />
+      )}
     </BaseView>
   );
 };
 
-export default connect()(Alarm);
+export default connect(({ alarm }) => ({
+  unreadList: alarm.unreadList,
+  readList: alarm.readList,
+  count: alarm.count
+}))(Alarm);
