338bb5fac29a1f1e8c7587fac0a91791b7afec06
juntae shin <wnsxo0816@gmail.com>
2019-08-02 21:13:28 +0900

    강제업데이트 화면 추가

--- src/navigations/auth/AuthSwitch.js
+++ src/navigations/auth/AuthSwitch.js
@@ -4,6 +4,7 @@ import UpdateCheck from "../../containers/update/UpdateCheck";
 import SignStack from "../sign/SignStack";
 import RootStack from "../root/root";
 import LockSolve from "../../containers/lock/LockSolve";
+import LibraryUpdate from "../../containers/update/LibraryUpdate";
 
 const AuthSwitch = createSwitchNavigator(
   {
@@ -11,10 +12,11 @@ const AuthSwitch = createSwitchNavigator(
     intro: { screen: Intro },
     update: { screen: UpdateCheck },
     sign: { screen: SignStack },
-    locksolve: { screen: LockSolve }
+    locksolve: { screen: LockSolve },
+    library: { screen: LibraryUpdate }
   },
   {
-    initialRouteName: "update"
+    initialRouteName: "library"
   }
 );
 
